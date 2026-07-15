2935698dc835526f923074f1aa5ac641d61672ec
StarAstronaut2 <zhir.l1874@gmail.com>
2026-07-15 17:29:10 +0800

    feat(sql): implement load_file document function

--- src/sql/engine/expr/ob_expr_eval_functions.cpp
+++ src/sql/engine/expr/ob_expr_eval_functions.cpp
@@ -388,6 +388,7 @@
 #include "sql/engine/expr/ob_expr_ai/ob_expr_ai_prompt.h"
 #include "ob_expr_vector_similarity.h"
 #include "ob_expr_check_location_access.h"
+#include "ob_expr_load_file.h"
 
 namespace oceanbase
 {
@@ -1341,6 +1342,7 @@ static ObExpr::EvalFunc g_expr_eval_functions[] = {
   ObExprVectorCosineSimilarity::calc_cosine_similarity,                /* 872 */
   ObExprVectorIPSimilarity::calc_ip_similarity,                        /* 873 */
   ObExprVectorSimilarity::calc_similarity,                             /* 874 */
+  ObExprLoadFile::eval_load_file,                                     /* 875 */
 };
 
 static ObExpr::EvalBatchFunc g_expr_eval_batch_functions[] = {
