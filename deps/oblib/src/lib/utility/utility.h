28858e37eb6c6d73198879632763bdfe6327ad54
dengfuping <fuping.dfp@antgroup.com>
2026-07-20 19:58:08 +0800

    [seekdb][build] define uint on Windows for utility.h embed build

--- deps/oblib/src/lib/utility/utility.h
+++ deps/oblib/src/lib/utility/utility.h
@@ -47,8 +47,10 @@ inline int usleep(useconds_t usec) {
 #include "lib/allocator/ob_malloc.h"
 #include "lib/time/ob_clock_generator.h"
 
-#ifdef __APPLE__
+#if defined(__APPLE__)
 #include <sys/types.h>  // includes BSD type definitions
+#endif
+#if defined(__APPLE__) || defined(_WIN32)
 using uint = unsigned int;
 #endif
 #define FALSE_IT(stmt) ({ (stmt); false; })
