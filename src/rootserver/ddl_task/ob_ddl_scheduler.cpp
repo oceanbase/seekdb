54f651a1b25f5b0a9b24b70aa211e2d3ec2d72c7
nijia.nj <nijia.nj@oceanbase.com>
2026-09-23 19:42:23 +0800

    Recover additional namespace table redefinition tasks

--- src/rootserver/ddl_task/ob_ddl_scheduler.cpp
+++ src/rootserver/ddl_task/ob_ddl_scheduler.cpp
@@ -2486,8 +2486,14 @@ int ObDDLScheduler::recover_task(const ObDDLTaskContext &context)
         if (context.namespace_id_ > 1
             && cur_record.ddl_type_ != DDL_CREATE_INDEX
             && cur_record.ddl_type_ != DDL_CREATE_PARTITIONED_LOCAL_INDEX
+            && cur_record.ddl_type_ != DDL_MODIFY_COLUMN
+            && cur_record.ddl_type_ != DDL_ADD_PRIMARY_KEY
+            && cur_record.ddl_type_ != DDL_DROP_PRIMARY_KEY
+            && cur_record.ddl_type_ != DDL_ALTER_PRIMARY_KEY
             && cur_record.ddl_type_ != DDL_TABLE_REDEFINITION
             && cur_record.ddl_type_ != DDL_ALTER_PARTITION_BY
+            && cur_record.ddl_type_ != DDL_CONVERT_TO_CHARACTER
+            && cur_record.ddl_type_ != DDL_MODIFY_AUTO_INCREMENT_WITH_REDEFINITION
             && cur_record.ddl_type_ != DDL_DROP_COLUMN
             && cur_record.ddl_type_ != DDL_ADD_COLUMN_OFFLINE
             && cur_record.ddl_type_ != DDL_COLUMN_REDEFINITION) {
